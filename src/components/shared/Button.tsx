0a3b6d3d9a67d624f730dc8e295909fdf9001b6e
eliyafazeli110 <eliyafazeli110@gmail.com>
2026-01-04 19:52:33 +0330

    feat : add Button and Input components

---
+++ src/components/shared/Button.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react"
+
+interface ButtonProps {
+  type: string
+  text: string
+}
+
+const Button: FC<ButtonProps> = ({ text }) => {
+  return (
+    <>
+      <button
+        type="submit"
+        className="w-full h-14 bg-blue-600 hover:bg-blue-700 transition-all duration-300 text-lg rounded-xl shadow-lg hover:shadow-blue-500/30"
+      >
+        {text}
+      </button>
+    </>
+  )
+}
+export default Button
---
+++ src/components/shared/Input.tsx
@@ -0,0 +1,44 @@
+import { FC } from "react"
+import { useField } from "formik"
+
+interface InputProps {
+  name: string
+  type?: string
+  placeholder?: string
+  errorClassName?: string
+  textForTitle?: string
+}
+
+const Input: FC<InputProps> = ({
+  name,
+  type = "text",
+  placeholder,
+  errorClassName,
+  textForTitle,
+}) => {
+  const [field, meta] = useField(name)
+
+  const hasError = meta.touched && meta.error
+
+  return (
+    <div className="w-full flex flex-col gap-1">
+      <h3 className="w-full text-start p-1">{textForTitle}</h3>
+      <input
+        {...field}
+        type={type}
+        placeholder={placeholder}
+        className={`w-full h-14 pl-10 pr-4 bg-gray-800 text-white border ${
+          hasError ? "border-red-500" : "border-gray-700"
+        } focus:outline-none focus:ring-2 focus:ring-blue-500 rounded-xl text-[16px]`}
+      />
+
+      {hasError && (
+        <div className={`font-Inter text-red-400 text-[14px] ${errorClassName ?? ""}`}>
+          {meta.error}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Input
